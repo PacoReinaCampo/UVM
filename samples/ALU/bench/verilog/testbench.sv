d799fb4a716cdac1a3aeb32d2166414efac3156c
PacoReinaCampo <frareicam@gmail.com>
2020-07-22 02:50:16 +0200

    new commit

--- samples/ALU/bench/verilog/testbench.sv
+++ samples/ALU/bench/verilog/testbench.sv
@@ -1,9 +1,49 @@
-`include "uvm_macros.svh"
-import uvm_pkg::*;
-`include "interface.sv"
-`include "test.sv"
+////////////////////////////////////////////////////////////////////////////////
+//                                            __ _      _     _               //
+//                                           / _(_)    | |   | |              //
+//                __ _ _   _  ___  ___ _ __ | |_ _  ___| | __| |              //
+//               / _` | | | |/ _ \/ _ \ '_ \|  _| |/ _ \ |/ _` |              //
+//              | (_| | |_| |  __/  __/ | | | | | |  __/ | (_| |              //
+//               \__, |\__,_|\___|\___|_| |_|_| |_|\___|_|\__,_|              //
+//                  | |                                                       //
+//                  |_|                                                       //
+//                                                                            //
+//                                                                            //
+//              Universal Verification Methodology                            //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
 
-module alutop;
+/* Copyright (c) 2020-2021 by the author(s)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * =============================================================================
+ * Author(s):
+ *   Paco Reina Campo <pacoreinacampo@queenfield.tech>
+ */
+
+`include "uvm_macros.svh"
+`include "uvm_pkg.sv"
+
+import uvm_pkg::*;
+
+module testbench;
  
   bit clk;
   bit reset;
--- samples/adder/bench/verilog/sequenceitems.sv
+++ samples/adder/bench/verilog/sequenceitems.sv
@@ -1,6 +1,43 @@
-//This is a file to create sequence items that will be the stimuli to the DUT
+////////////////////////////////////////////////////////////////////////////////
+//                                            __ _      _     _               //
+//                                           / _(_)    | |   | |              //
+//                __ _ _   _  ___  ___ _ __ | |_ _  ___| | __| |              //
+//               / _` | | | |/ _ \/ _ \ '_ \|  _| |/ _ \ |/ _` |              //
+//              | (_| | |_| |  __/  __/ | | | | | |  __/ | (_| |              //
+//               \__, |\__,_|\___|\___|_| |_|_| |_|\___|_|\__,_|              //
+//                  | |                                                       //
+//                  |_|                                                       //
+//                                                                            //
+//                                                                            //
+//              Universal Verification Methodology                            //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
 
-//Creation of a seq_items class that extends the parent UVM class
+/* Copyright (c) 2020-2021 by the author(s)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * =============================================================================
+ * Author(s):
+ *   Paco Reina Campo <pacoreinacampo@queenfield.tech>
+ */
+
 class seq_item extends uvm_sequence_item; 
   
   //Registration of the DUT signals and its data type in the UVM Factory 
@@ -19,6 +56,6 @@ class seq_item extends uvm_sequence_item;
     super.new(name);
   endfunction             
   
-  /*These contraints are added for even the randomization of B to be in this range. This can be tweaked in various ways.*/
+  //These contraints are added for even the randomization of B to be in this range. This can be tweaked in various ways
   constraint constr{B inside {3,6,8,9,10,25};}; 
 endclass
